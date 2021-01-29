0fd5c0b5614285b34d2dc5d2e05f9cb577780f12
Lillian Won <lwon1@uci.edu>
2021-01-29 11:33:51 -0800

    Adding low-quality/traps

--- scraper.py
+++ scraper.py
@@ -2,26 +2,26 @@ import re
 from urllib.parse import urlparse, urldefrag, urljoin
 from bs4 import BeautifulSoup as bs
 
-whitelist = set()
-blacklist = set() 
-
-import requests
+import requests 
 
 def scraper(url, resp): # will receive a URL and the response given by the caching server for the requested URL (the webpage) 
-    links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)] # scrapped list of URLs from the page 
+    # links = extract_next_links(url, resp)
+    if resp.status in [200, 201, 202, 203, 205, 206]:
+        links = extract_next_links(url,resp)
+        return [link for link in links if is_valid(link)] # scrapped list of URLs from the page 
 
+    return list()
+
+# only focus on resp.status 200-203, 205 & 206
 def extract_next_links(url, resp):
     # links to return  
     links = []
 
-
-    # 204 = Response successful, no content
-    if ( resp.status < 200 or resp.status > 400 or resp.status == 204): 
-        return list()# these response are bad
-
     # resp.raw_response.content will give content of HTML webpage     
     # some web pages will not have a raw_response
+
+    if resp.raw_response is None:
+        return list()
 
     # Beautiful soup will do it's magic and extract data into lmxl 
     # and then helps us get all the tags from lxml file
@@ -34,7 +34,11 @@ def extract_next_links(url, resp):
         temp = anchor_tag.get('href') 
         defrag,_ = urldefrag(temp)
         # creates absolute paths 
-        links.append(urljoin(url, defrag))
+
+        if is_valid(urljoin(url,defrag)):
+            links.append(urljoin(url,defrag))
+
+
 
     return list(links) 
 
@@ -44,6 +48,7 @@ def extract_next_links(url, resp):
 def is_valid(url):
     try:
         parsed = urlparse(url)
+
         if parsed.scheme not in set(["http", "https"]):
             return False
 
@@ -51,32 +56,49 @@ def is_valid(url):
         if not re.match(r"^(\w*.)(ics.uci.edu|cs.uci.edu|informatics.uci.edu|stat.uci.edu)", parsed.netloc):
             return False
 
-        #  some websites have PDF in middle of url = becomes a trap
-        # must re.match or something 
-        # Ex of trap in middle of URL: Downloaded http://www.informatics.uci.edu/files/pdf/InformaticsBrochure-March2018
-        if "pdf" in url:
-            return False
-        
-        # query strings that aren't being detected.. from wics pages
+        if re.match(r"^(today.uci.edu)", parsed.netloc ):
+            if not re.match(r"^(\/department/information_computer_sciences\/).*", parsed.path):
+                return False
+
+        # aren't being detected.. from wics pages
         if "?action" in url:
             return False
 
-        # blog posts/comments traps
-        if re.match(r"(#comment|#respond|\?replytocom)", parsed.path):
+        # ?replytocom from evoke website.. we already see the comments in the evoke link 
+        if re.match(r".*(\?replytocom).*$", parsed.path):
             return False
 
         # ?share= from wics share with google, twitter, etd..
         if "?share=" in url:
             return False
 
-        # doku.php from swiki and wp-content = img, event(s) from wics calendar pg 
+        # wp-content = evoke website 
+
+        # doku.php from swiki and wp-content = img , event(s) from wics calendar pg 
         # ....eppstein/pix/ so many.. pictures.. horrible.
-        if re.match(r"(/doku.php/|/wp-content/|/pix/|/events/|/event/)", parsed.path):
+        # looks for all events, so hack.uci events arent seen
+        if re.search(r"(/doku.php/|/pix/|/events/|/event/)", parsed.path):
             return False 
+        
+        # block any url that contains calendar
+        if re.match(r"^.*calendar.*$", parsed.path):
+            return False
 
+        #  some websites have PDF in middle of url = becomes a trap
+        # must re.match or something 
+        if re.match(
+            r".*(css|js|bmp|gif|jpe?g|ico"
+            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx"
+            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz).*$", parsed.path):
+            return False
 
-        # this only checks for urls ending in these extensions
-        # added php, ppsx, odc, apk extensions
+        # only removes url with paths ending in these extensions
+        # added php, ppsx, odc, apk,  extensions
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -85,8 +107,9 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv|z|php"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ppsx|odc|apk)$", parsed.path.lower())
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ppsx|odc|apk|war|img)$", parsed.path.lower())
 
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
